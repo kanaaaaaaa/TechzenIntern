3d254d052b837fe8aa1060c838ad2e1b4cdac0ac
Itabashi Masaki <anothersupermariobros@gmail.com>
2026-09-04 15:38:38 +0700

    Update StatusControl.vue

--- frontend/src/components/StatusControl.vue
+++ frontend/src/components/StatusControl.vue
@@ -6,9 +6,9 @@ defineProps({
 defineEmits(["update:modelValue"])
 
 const options = [
-  { value: "accepted", label: "Accepted" },
-  { value: "not_accepted", label: "Not accepted" },
-  { value: "unknown", label: "Unknown" },
+  { value: "⚪︎", label: "Accepted" },
+  { value: "×", label: "Not accepted" },
+  { value: "？", label: "Unknown" },
 ]
 </script>
 
